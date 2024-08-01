3f399a12ed682bd4a343966e4791876b25b6f64f
Dominique Duclayan <67725825+duclayan@users.noreply.github.com>
2024-08-01 08:43:40 +0000

    function sample 1

--- function_app.py
+++ function_app.py
@@ -1,25 +1,18 @@
+from datetime import datetime
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+#Function Example 1 : HTTP Trigger
+#Functions work with decorators
+@app.function_name(name='TimeZoneHttpTrigger')
+@app.route(route='timezone', auth_level = func.AuthLevel.ANONYMOUS)
+def TimeZoneHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    """
+    logging.info('Timezone HTTP trigger function received a request')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    timezone = req.get_json['timezone']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    print(timezone)
